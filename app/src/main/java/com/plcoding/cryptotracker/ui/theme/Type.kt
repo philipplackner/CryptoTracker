e1733911d9e92f5f6ab680b6c4075b198c09e1b9
Philipp Lackner <mail@pl-coding.com>
2024-09-20 19:45:58 +0200

    Initial commit

--- app/src/main/java/com/plcoding/cryptotracker/ui/theme/Type.kt
+++ app/src/main/java/com/plcoding/cryptotracker/ui/theme/Type.kt
@@ -2,33 +2,60 @@ package com.plcoding.cryptotracker.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.plcoding.cryptotracker.R
+
+val SpaceMono = FontFamily(
+    Font(
+        resId = R.font.space_mono_regular,
+        weight = FontWeight.Normal
+    ),
+    Font(
+        resId = R.font.space_mono_italic,
+        weight = FontWeight.Normal,
+        style = FontStyle.Italic
+    ),
+    Font(
+        resId = R.font.space_mono_bold,
+        weight = FontWeight.Bold
+    ),
+    Font(
+        resId = R.font.space_mono_bold_italic,
+        weight = FontWeight.Bold,
+        style = FontStyle.Italic
+    ),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    bodySmall = TextStyle(
+        fontFamily = SpaceMono,
+        fontWeight = FontWeight.Light,
+        fontSize = 12.sp,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = SpaceMono,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = SpaceMono,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    labelMedium = TextStyle(
+        fontFamily = SpaceMono,
+        fontWeight = FontWeight.Normal,
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = SpaceMono,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
     )
-    */
 )
